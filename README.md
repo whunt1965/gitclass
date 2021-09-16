2d99ebbc6ccd3d23abe42f00da24620ae2045f7f
Wiley Hunt <whunt@bu.edu>
2021-09-16 11:45:40 -0400

    Revert "Update README.md"
    This reverts commit da35883fe5b8d64cc944ecf6cf8b7eafc9610651.
    
    Add function
    
    Subtract function
    
    Squash test

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # gitclass
-test project for git. Completed 9/16.
+test project for git
--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
 def say_hello():
     print("Hello World!")
 
+def add(x, y):
+    return x+y
+
+def subtract(x, y):
+    return x-y
+
 if __name__=="__main__":
     say_hello()
+    print("The sum of 1 and 2 is", add(1, 2))
+    print("The difference of 1 and 2 is", subtract(1, 2))
